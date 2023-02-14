018db34b0d1b73ff40351541514c32ca282a2302
Douglas Alves <douglas_dca@hotmail.com>
2023-02-14 14:21:14 -0300

    refactor: CarService now has a CarODM attribute to be used by all CarService methods.

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -3,14 +3,19 @@ import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarODM';
 
 export default class CarService {
+  private carODM: CarODM;
+
+  constructor() {
+    this.carODM = new CarODM();
+  }
+
   private createCarDomain(car: ICar | null): Car | null {
     if (car) return new Car(car);
     return null;
   }
 
   public async insertCar(car: ICar) {
-    const carODM = new CarODM();
-    const newCar = await carODM.create(car);
+    const newCar = await this.carODM.create(car);
     return this.createCarDomain(newCar);
   }
 }
